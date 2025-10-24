e3ffb2b4ef5baf57ebd3f2322db243950e69b8d6
Clarine99 <dericaku.id@gmail.com>
2025-10-24 09:31:49 +0800

    first commit

---
+++ kopihub_crm/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class KopihubCrmConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'kopihub_crm'
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from kopihub-crm!")
+
+
+if __name__ == "__main__":
+    main()
